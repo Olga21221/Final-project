487b46d0829b14f40d4e0b7543bf5bae162d35d3
Olga <olgaolga21221@mail.ru>
2023-02-24 17:58:56 +0300

    Добавили решение задачи

---
+++ Task001/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+string [] array = {"Hello", "1234", "125", "World", "13", "0", "xx"};
+
+
+void GetArray(string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        char[] c = array[i].ToCharArray();
+        for (int j = 0; j < c.Length;)
+        {
+            if(c.Length <= 3)
+            {
+                Console.WriteLine(c);
+                break;
+            }
+            else break;
+        }
+    }
+}
+
+GetArray(array);
